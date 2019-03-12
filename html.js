c544ff45ccaa62b86f1573f1b7c5062e540334ee
tylodung <39002385+tylodung@users.noreply.github.com>
2019-03-12 17:33:47 +0700

    up

--- html.js
+++ html.js
@@ -70,7 +70,7 @@ function buildGoogleAnalyticsSetup() {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-3328305077552977',
+    google_ad_client: 'ca-pub-5299967500346791',
     enable_page_level_ads: true
   });
   `;
