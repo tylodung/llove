bc2c65969a74e796f20d9f18f57901a72030fb16
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-07-03 21:14:19 +0700

    up

--- html.js
+++ html.js
@@ -70,7 +70,7 @@ function buildGoogleAnalyticsSetup() {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-6020662213494913',
+    google_ad_client: 'ca-pub-idhere',
     enable_page_level_ads: true
   });
   `;
