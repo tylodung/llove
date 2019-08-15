6ac6e56d21c1a4a53a6de5a471aa3993e523b670
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-08-15 17:58:58 +0700

    up

--- components/SideBar/index.jsx
+++ components/SideBar/index.jsx
@@ -13,8 +13,8 @@ class SideBar extends React.Component {
 		<div className='sidebar__sponsor'>Sponsor</div>
                 <div className='sidebar__title'>Blogs</div>
 		<div className='sidebar__link'>
-		<a href='/about'>Đoàn Bình & Siêu Thị Mỹ Phẩm</a>
-		<a href='http://haboba.com'>Điều Cần Biết Khi Dùng Mỹ Phẩm</a>
+		<a href='/about'>Đoàn Bình & Bách Hoá Mỹ Phẩm</a>
+		<a href='https://goclamdep.xyz'>Điều Cần Biết Khi Dùng Mỹ Phẩm</a>
 		<a href='/payments'>Thanh toán</a>
 		<a href='/help'>Hướng dẫn đặt mua</a>
 		</div>
